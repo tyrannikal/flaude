55ab5165093db4e0cd5075ef39a29259bac712e4
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2026-04-21 19:26:06 -0400

    add verbose output flag

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ from google.genai import types
 def main():
     print("Hello from flaude!")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     assert api_key is not None, "GEMINI_API_KEY is not set"
 
     client = genai.Client(api_key=api_key)
 
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-    # Now we can access `args.user_prompt`
-
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    print(f"User prompt: {args.user_prompt}")
+    generate_response(client, messages, args.verbose)
 
+
+def generate_response(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
@@ -30,8 +32,10 @@ def main():
         "Response usage_metadata is None, likely failed api call"
     )
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {messages[0].parts[0].text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print(f"Response: {response.text}")
 
