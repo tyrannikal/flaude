fc07bdfb45abc1f14e401f3fc74880cb234b8e77
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2026-04-20 22:54:43 -0400

    include token metadata in response flow

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
@@ -12,11 +13,25 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    print(f"User prompt: {args.user_prompt}")
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=args.user_prompt,
     )
-    print(response.text)
+    assert response.usage_metadata is not None, (
+        "Response usage_metadata is None, likely failed api call"
+    )
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
